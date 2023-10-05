05b5b6f205489edd9381fb4f7365888e6df090c8
AliRaouf <ali_r00919@cic-cairo.com>
2023-10-05 12:45:07 +0300

    last

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -16,84 +16,88 @@ class _HomeScreenState extends State<HomeScreen> {
     return BlocConsumer<IdCubit, IdState>(
         builder: (context, state) {
           return Scaffold(
-            body: Stack(
+            body: ListView(
               children: [
-                SizedBox(
-                    width: screenwidth,
-                    height: screenheight,
-                    child: const Image(
-                        image: AssetImage('assets/images/homepage.png'),
-                        fit: BoxFit.cover)),
-                Column(
+                Stack(
                   children: [
-                    Center(
-                      child: Container(
-                        margin: EdgeInsets.only(
-                            top: screenheight * 0.45,
-                            bottom: screenheight * 0.04),
-                        width: screenwidth * 0.9,
-                        height: screenheight * 0.1,
-                        child: TextField(
-                          decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.white.withOpacity(0.7),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(45),
-                                  borderSide: BorderSide.none),
-                              hintText: "Name",
-                              hintStyle: TextStyle(color: Colors.grey[600])),
-                          onChanged: (name) {
-                            cubit.userName = name;
-                          },
+                    SizedBox(
+                        width: screenwidth,
+                        height: screenheight,
+                        child: const Image(
+                            image: AssetImage('assets/images/homepage.png'),
+                            fit: BoxFit.cover)),
+                    Column(
+                      children: [
+                        Center(
+                          child: Container(
+                            margin: EdgeInsets.only(
+                                top: screenheight * 0.45,
+                                bottom: screenheight * 0.04),
+                            width: screenwidth * 0.9,
+                            height: screenheight * 0.1,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white.withOpacity(0.7),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(45),
+                                      borderSide: BorderSide.none),
+                                  hintText: "Name",
+                                  hintStyle: TextStyle(color: Colors.grey[600])),
+                              onChanged: (name) {
+                                cubit.userName = name;
+                              },
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(bottom: screenheight * 0.05),
-                      width: screenwidth * 0.9,
-                      height: screenheight * 0.1,
-                      child: TextFormField(
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white.withOpacity(0.7),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(45),
-                                borderSide: BorderSide.none),
-                            hintText: "National ID",
-                            hintStyle: TextStyle(color: Colors.grey[600])),
-                        onChanged: (number) {
-                          cubit.nationalId = number;
-                        },
-                      ),
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          cubit.identifyBirthYear();
-                          cubit.identifyBirthMonth();
-                          cubit.identifyBirthDay();
-                          cubit.identifyGovernance();
-                          cubit.identifyGender();
-                          if(cubit.nationalId.length!=14){
-                            null;
-                          }else{
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => IDScreen()));}
-                          },
-                        style: const ButtonStyle(
-                            backgroundColor:
-                                MaterialStatePropertyAll(Colors.white),
-                            side: MaterialStatePropertyAll(BorderSide(
-                                width: 5,
-                                style: BorderStyle.solid,
-                                color: Color(0xff3c585b)))),
-                        child: const Text(
-                          "Analyse user data",
-                          style: TextStyle(color: Color(0xff3c585b)),
-                        ))
+                        Container(
+                          margin: EdgeInsets.only(bottom: screenheight * 0.05),
+                          width: screenwidth * 0.9,
+                          height: screenheight * 0.1,
+                          child: TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.white.withOpacity(0.7),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(45),
+                                    borderSide: BorderSide.none),
+                                hintText: "National ID",
+                                hintStyle: TextStyle(color: Colors.grey[600])),
+                            onChanged: (number) {
+                              cubit.nationalId = number;
+                            },
+                          ),
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              cubit.identifyBirthYear();
+                              cubit.identifyBirthMonth();
+                              cubit.identifyBirthDay();
+                              cubit.identifyGovernance();
+                              cubit.identifyGender();
+                              if(cubit.nationalId.length!=14){
+                                null;
+                              }else{
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => IDScreen()));}
+                              },
+                            style: const ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll(Colors.white),
+                                side: MaterialStatePropertyAll(BorderSide(
+                                    width: 5,
+                                    style: BorderStyle.solid,
+                                    color: Color(0xff3c585b)))),
+                            child: const Text(
+                              "Analyse user data",
+                              style: TextStyle(color: Color(0xff3c585b)),
+                            ))
+                      ],
+                    )
                   ],
-                )
+                ),
               ],
             ),
           );
